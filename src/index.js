33e761374d3a0e02caa1976c8da7b07ddd5a116a
odev954 <kleinbottle175@gmail.com>
2022-09-13 17:18:28 +0300

    adding imports to index, setup to testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 function main()
 {
+    container = document.getElementById('mainPanel');
 
+    conatiner.appendChild(TaskStatusButton().render(
+        {
+            ongoing: '',
+            done: ''
+        }
+    ));
 }
 
 main()
