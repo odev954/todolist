7d9e859064630fdaa37e8dfe9b817c9ee4c954e1
odev954 <kleinbottle175@gmail.com>
2022-09-14 17:53:35 +0300

    removing attributes from element, binding to flag, improving overall design of status button

--- src/components/TaskStatusButton/TaskStatusButton.js
+++ src/components/TaskStatusButton/TaskStatusButton.js
@@ -2,30 +2,31 @@ function TaskStatusButton()
 {
     return {
         completed: false,
+        components: {},
+        styles: {},
         render: function (styles) {
             let classList = styles['ongoing']; //select style
             let button = document.createElement('button');
-            
-            button.setAttribute('completed', 'false');
-            button.setAttribute('styles', JSON.stringify(styles));
-            button.addEventListener('click', this.click);
+
+            button.addEventListener('click', (e) => {
+                let classList = this.styles[!this.completed ? 'done' : 'ongoing'];
+                
+                this.completed = !this.completed;
+    
+                this.components.button.classList = ''; //clear styles
+                this.components.button.classList.add(...classList);
+    
+                this.components.button.textContent = !this.completed ? 'mark as done' : 'mark as ongoing';
+            });
+
             button.classList.add(...classList); //style component
 
             button.textContent = 'mark as done';
 
-            return button;
-        },
-        click: function() {
-            let completed = this.getAttribute('completed') === 'true';
-            let styles = JSON.parse(this.getAttribute('styles'));
-            let classList = styles[!completed ? 'done' : 'ongoing'];
+            this.components.button = button;
+            this.styles = styles;
 
-            this.setAttribute('completed', (!completed).toString());
-
-            this.classList = ''; //clear styles
-            this.classList.add(...classList);
-
-            this.textContent = completed ? 'mark as done' : 'mark as ongoing';
+            return this.components.button;
         }
     }
 }
