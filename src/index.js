0adc57878dfd997391c68f6fb15b6ca04cce7a6f
odev954 <kleinbottle175@gmail.com>
2022-09-14 14:14:38 +0300

    importing task styles to index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function main()
 {
     let container = document.getElementById('test');
     let test = Task('testing this shit', 'test', new Date(Date.now()))
-            .render(container.id, []);
+            .render(container.id, ['task-card']);
     //container.append(test);
 }
 
