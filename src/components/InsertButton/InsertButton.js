3296f39b44a7c35f79259b156b400fe604136758
odev954 <kleinbottle175@gmail.com>
2022-09-15 14:30:46 +0300

    adding new component: insert button

---
+++ src/components/InsertButton/InsertButton.js
@@ -0,0 +1,28 @@
+function InsertButton(listContainer, todoList)
+{
+    return {
+        todoList: todoList,
+        listContainer: listContainer,
+        components: {},
+        render: function (classList) {
+            let button = document.createElement('button');
+
+            button.addEventListener('click', (e) => {
+                listContainer.append(
+                    todoList.add(
+                        Task('Do something...', 'New Task', new Date(Date.now()))
+                    )
+                );
+                todoList.components.title.focus();
+            });
+
+            button.classList.add(...classList); //style component
+
+            button.textContent = 'Add new task';
+
+            this.components.button = button;
+
+            return this.components.button;
+        }
+    }
+}
