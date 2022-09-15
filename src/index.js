4418ba1b2b55c2c46d4c14858a4d8bd33d611d65
odev954 <kleinbottle175@gmail.com>
2022-09-15 13:03:24 +0300

    setup to task list test ground

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 function main()
 {
     let container = document.getElementById('test');
-    let test = Task('testing this shit', 'test', new Date(Date.now()))
-            .render(container.id, ['task-card']);
-    //container.append(test);
+    let test1 = Task('testing this shit', 'test1', new Date(Date.now()));
+    let test2 = Task('testing this shit', 'test2', new Date(Date.now()));
+    let test3 = Task('testing this shit', 'test3', new Date(Date.now()));
+    let list = TaskList();
+
+    
+    container.append(list.render(['cards-grid']));
+    
+    list.add(test1);
+    list.add(test2);
+    let element = list.add(test3);
+    
+    container.append(element);
+
 }
 
 main()
