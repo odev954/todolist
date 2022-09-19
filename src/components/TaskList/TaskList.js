0bee72a1ff7b537e1a1303b9fe8ddbf811d14e91
odev954 <kleinbottle175@gmail.com>
2022-09-19 15:51:32 +0300

    resolving bugs with event firing

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,6 +2,8 @@ function TaskList()
 {
     return {
         idCounter: 0,
+        dragTarget: '',
+        dragSource: '',
         tasks: [],
         components: {},
         add: function(task) {
@@ -26,24 +28,34 @@ function TaskList()
             this.tasks.splice(destinationPosition, 0, this.tasks[sourcePosition]);
             this.tasks.splice(sourcePosition + 1, 1);
 
-            document.getElementById(this.components.container.id)?.remove();
             return this.render(this.components.container.classList);
         },
         render: function(classList) {
             let container = document.createElement('div');
 
             container.setAttribute('id', 'todolist');
-            this.tasks.forEach(function(item) {
+            this.tasks.forEach((item) => {
                 let renderedTask = item.task.render(['task-card']);
                 
                 renderedTask.setAttribute('id', item.id);
 
-                renderedTask.addEventListener("dragenter", (event, target) => {
-                    this.onTaskDrop(event, target);
+                renderedTask.addEventListener('dragstart', (event) => {
+                    this.dragSource = event.target.id;
+                });
+
+                renderedTask.addEventListener("dragenter", (event) => {
+                    this.dragTarget = event.target.id; 
+                    console.log('Drag enter: ' + this.dragTarget); 
+                    event.preventDefault();                 
                 });
                 
-                renderedTask.addEventListener("dragover", (event, target) => {
-                    this.onTaskDrop(event, target);
+                renderedTask.addEventListener("dragover", (event) => {
+                    event.preventDefault();             
+                });
+                
+                renderedTask.addEventListener("drop", (event) => {
+                    console.log('Drag drop: ' + this.dragTarget);
+                    this.onTaskDrop(event);
                 });
                 
                 container.append(renderedTask);
@@ -55,10 +67,14 @@ function TaskList()
 
             return this.components.container;
         },
-        onTaskDrop: function (event, target) {
-            const renderedList = this.reposition(event.target.id, target.id);
+        onTaskDrop: function (event) {
+            console.log("target: " + this.dragSource + 
+            "\ntransferred: " + this.dragTarget);
+            const renderedList = this.reposition(this.dragSource, this.dragTarget);
+            const parentNode = document.getElementById(this.components.container.id)?.parentNode;
 
-            this.components.container.parentElement.append(renderedList);
+            document.getElementById(this.components.container.id)?.remove();
+            parentNode.append(renderedList);
             
             event.preventDefault();
         }
