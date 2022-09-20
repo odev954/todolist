5de4f16a968679432ebe333ede20967deab3ea46
odev954 <kleinbottle175@gmail.com>
2022-09-20 18:01:00 +0300

    adding keypress detection

--- src/components/InsertButton/InsertButton.js
+++ src/components/InsertButton/InsertButton.js
@@ -4,20 +4,26 @@ function InsertButton(listContainer, todoList)
         todoList: todoList,
         listContainer: listContainer,
         components: {},
+        insertTask:  () => {
+            listContainer.append(
+                todoList.add(
+                    Task('Do something...', 
+                        'New Task', 
+                        new Date(Date.now()), 
+                        completed=false, 
+                        taskList=todoList)
+                )
+            );
+        },
         render: function (classList) {
             let button = document.createElement('button');
 
-            button.addEventListener('click', (e) => {
-                listContainer.append(
-                    todoList.add(
-                        Task('Do something...', 
-                            'New Task', 
-                            new Date(Date.now()), 
-                            completed=false, 
-                            taskList=todoList)
-                    )
-                );
-            });
+            button.addEventListener('click', this.insertTask);
+            document.addEventListener('keydown', (event) => {
+                if(event.key == 'Enter') {
+                    this.insertTask();
+                }
+            })
 
             button.classList.add(...classList); //style component
 
--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -2,6 +2,7 @@ function TextField(text='', taskList=null)
 {
     return {
         text: text,
+        newText: text,
         taskList: taskList,
         components: {},
         render: function(tag='p', classList=[]) {
@@ -13,9 +14,24 @@ function TextField(text='', taskList=null)
             textField.innerHTML = text;
 
             textField.addEventListener("input", (e) => {
-                this.text = e.target.innerHTML;
-                this.taskList?.save();
+                this.newText = e.target.innerHTML;
             });
+            textField.addEventListener('keydown', (event) => {
+                if(event.key == 'Tab') {
+                    this.text = this.newText;
+                    this.components.textField.innerHTML = this.text;
+                    this.taskList?.save();
+                }
+                else if(event.key == 'Escape') {
+                    this.newText = this.text;
+                    this.components.textField.innerHTML = this.text;
+                }
+            });
+            textField.addEventListener('focusout', () => {
+                this.newText = this.text;
+                this.components.textField.innerHTML = this.text;
+            });
+            
 
             this.components.textField = textField;
 
