d8d947f65b24a8ad8d4b32b815c6f2bb159c0e94
odev954 <kleinbottle175@gmail.com>
2022-09-20 14:45:24 +0300

    setting up main menu for options

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 function main()
 {
-    localStorage.clear();
+    //localStorage.clear();
     let container = document.getElementById('test');
-    let test1 = Task('testing this shit', 'test1', new Date(Date.now()));
-    let test2 = Task('testing this shit', 'test2', new Date(Date.now()));
-    let test3 = Task('testing this shit', 'test3', new Date(Date.now()));
+    let menu = document.getElementById('menu');
+    //let test1 = Task('testing this shit', 'test1', new Date(Date.now()));
+    //let test2 = Task('testing this shit', 'test2', new Date(Date.now()));
+    //let test3 = Task('testing this shit', 'test3', new Date(Date.now()));
     let list = TaskList();
     let ins = InsertButton(container, list);
     let rmv = RemoveButton(container, list);
     
     container.append(list.render(['cards-grid']));
     
-    list.add(test1);
-    list.add(test2);
-    let element = list.add(test3);
+    //list.add(test1);
+    //list.add(test2);
+    //let element = list.add(test3);
     
     
-    container.append(element);
-    container.append(ins.render(['insert-btn', 'text']));
-    container.append(rmv.render(['remove-btn', 'text']));
+    //container.append(element);
+    menu.append(ins.render([]));
+    menu.append(rmv.render([]));
     
     ins.components.button.addEventListener('click', () => rmv.setupClickEvents());
 }
